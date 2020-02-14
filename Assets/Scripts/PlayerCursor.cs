9a0c505945474838d6098b37aab6657da70e9f82
Muzaffer Kadir YILMAZ <muzafferkadiryilmaz@gmail.com>
2020-02-14 03:36:39 +0300

    Cursor bug
    cursor can visible and unvisible now

---
+++ Assets/Scripts/PlayerCursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    private void OnDisable()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
--- Assets/Scripts/PlayerMouseControl.cs
+++ Assets/Scripts/PlayerMouseControl.cs
@@ -7,12 +7,6 @@ public class PlayerMouseControl : NetworkBehaviour
     public float mouseSensivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
-    void Start()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
@@ -21,10 +15,5 @@ public class PlayerMouseControl : NetworkBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
-    }
-    private void OnDisable()
-    {
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.None;
     }
 }
