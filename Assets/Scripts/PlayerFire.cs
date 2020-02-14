55e24dea1c38202e619e0eb2b2d7f2ab0fbc9654
Muzaffer Kadir YILMAZ <muzafferkadiryilmaz@gmail.com>
2020-02-14 04:55:34 +0300

    Player Fire Script
    Multiplayer Can Fire

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerFire : NetworkBehaviour
+{
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    public ParticleSystem gunFirePartical;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CmdFire();
+        }
+    }
+    [Command]
+    private void CmdFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 2);
+    }
+}
